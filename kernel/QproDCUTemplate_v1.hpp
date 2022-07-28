fc170313363607f1260216e0e0d073180ef71095
Rhythmicc <RhythmLian@outlook.com>
2022-07-29 02:47:18 +0800

    update

--- kernel/QproDCUTemplate_v1.hpp
+++ kernel/QproDCUTemplate_v1.hpp
@@ -1,12 +1,9 @@
 #include <kernel_common.hpp>
 
-
-__global__ void _QproDCUTemplate() {
-    return;
-}
+__global__ void _QproDCUTemplate() { return; }
 
 float QproDCUTemplate() {
-    float gflops = 0f;
-    _QproDCUTemplate<<<1,1>>>();
-    return gflops;
+  float gflops = 0.f;
+  _QproDCUTemplate<<<1, 1>>>();
+  return gflops;
 }
